3c071e67c1f1978009df11f4ca11531a84561945
Ruben Teixeira <RubenTeixeira@users.noreply.github.com>
2024-08-09 21:36:35 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,4 +1,8 @@
 #!/system/bin/sh
+sleep 120
+MODDIR=${0%/*}
 cat /system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini &>/dev/null
+cat "$MODDIR/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini" &>/dev/null
 ifconfig wlan0 down
+sleep 3
 ifconfig wlan0 up
