a82f8e860fb65599fe34b207ad0f5664cc501207
Ruben Teixeira <RubenTeixeira@users.noreply.github.com>
2024-08-12 16:45:25 +0100

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,3 +1,2 @@
-#!/system/bin/sh
 MODDIR=${0%/*}
 ( sleep 300 && cat /system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini &>/dev/null && cat "$MODDIR/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini" &>/dev/null && svc wifi disable && svc wifi enable ) &
