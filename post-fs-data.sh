4ef11e3a830416e3f3d865f48b2e1a687176c917
Ruben Teixeira <RubenTeixeira@users.noreply.github.com>
2024-09-04 14:45:56 +0100

    Update and rename service.sh to post-fs-data.sh

---
+++ post-fs-data.sh
@@ -0,0 +1,2 @@
+MODDIR=${0%/*}
+( sleep 300 ; { cat /system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini &>/dev/null ; cat "$MODDIR/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini" &>/dev/null ; } ; svc wifi disable && sleep 2 && svc wifi enable ) &
--- service.sh
+++
@@ -1,2 +0,0 @@
-MODDIR=${0%/*}
-( sleep 300 ; { cat /system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini &>/dev/null ; cat "$MODDIR/system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini" &>/dev/null ; } ; svc wifi disable && svc wifi enable ) &
